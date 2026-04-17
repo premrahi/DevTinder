fd3360663bfb81206eb36b3aea45c27b1b13f604
premrahi <premrahi234@gmail.com>
2026-04-18 00:13:34 +0530

    params

--- src/app.ts
+++ src/app.ts
@@ -6,24 +6,11 @@ const app = express();
 
 
 
-app.get("/test", (req: Request, res: Response) => {
+app.get("/user/:userID/:userName/:password", (req: Request, res: Response) => {
+    console.log(req.params) ;
     res.send("test test test!");
 });
 
-app.post("/posting" , async (req: Request , res: Response) => {
-    res.send("this is a post api call")
-})
-
-app.get("/hello", (req: Request, res: Response) => {
-    res.send("hello hello hello!");
-});
-
-app.delete("/del", (req: Request, res: Response) => {
-    res.send("bye bye bye!");
-});
-app.get("/", (req: Request, res: Response) => {
-    res.send("hello from the dashboard!");
-});
 
 
 app.listen(3000, () => {
