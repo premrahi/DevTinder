90d30a8bc710f092a5b4585d67e3b02832260f63
premrahi <premrahi234@gmail.com>
2026-05-03 01:09:51 +0530

    multiple route-handlers

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,37 @@
-const express = require('express');
-import { Request, Response } from 'express';
-
+const express = require("express");
+import { NextFunction, Request, Response } from "express";
 
 const app = express();
 
+app.get(
+  "/user",
+  (req: Request, res: Response, next: NextFunction) => {
+    //Route handler
+    // res.send("Route handler 1");
+    next();
+  },
+  (req: Request, res: Response, next: NextFunction) => {
+    // res.send("Route handler 2");
+    next();
+  },
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send("Route handler 3");
+    next();
+  },
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send("Route handler 4");
+    next();
+  },
 
-
-app.get("/user/:userID/:userName/:password", (req: Request, res: Response) => {
-    console.log(req.params) ;
-    res.send("test test test!");
-});
-
-
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send("Route handler 5");
+    next();
+  },
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send("Route handler 6");
+  }
+);
 
 app.listen(3000, () => {
-    console.log("Server is successfully listening on port 3000");
+  console.log("Server is successfully listening on port 3000");
 });
