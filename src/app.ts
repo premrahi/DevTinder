0c41fcd54e666972395eafd8e596244e48a11fb9
premrahi <premrahi234@gmail.com>
2026-05-08 17:53:41 +0530

    error handling

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,18 @@
 const express = require("express");
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response, ErrorRequestHandler } from "express";
 
 const app = express();
 
-const {adminAuth , UserAuth} = require("./middlewares/auth")
-
-// this will take care of all the auth middle ware for GET,POST... 
-app.use("/admin" ,adminAuth );
-
-app.get("/user" , UserAuth ,(req:Request , res:Response)=> {
-    res.send("user data send") ;
-})
-
-app.get("/admin/getAllData" , (req:Request ,res:Response,next:NextFunction)=>{
-    res.send("got all the Data") ;
+app.get("/getUserData", (req: Request, res: Response) => {
+  throw new Error("nfnnao");
+  res.send("user data sent");
 });
 
-app.get("/admin/DeleteUser" , (req:Request ,res:Response,next:NextFunction)=>{
-    res.send("Deleted a user") ;
+app.use("/", (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
-
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
